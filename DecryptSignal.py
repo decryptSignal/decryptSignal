891bf8a1e6ca1a8d67f764b1912906db45251336
decryptSignal <60512749+decryptSignal@users.noreply.github.com>
2021-05-07 17:29:10 +0200

    Add files via upload

---
+++ DecryptSignal.py
@@ -0,0 +1,305 @@
+import PySimpleGUI as sg
+import sys
+import os
+import plistlib
+from pysqlcipher3 import dbapi2 as sqlcipher
+import report
+import json
+
+os_version = ""
+
+def main_menu():
+    global os_version
+    global attachmentPath
+    layout = [
+        [sg.Text('Choose database source Operating System')],
+        [sg.Radio("iOS", "os_input", default=True, key="ios"),
+         sg.Radio("Windows", "os_input", default=False, key="windows")],
+        [sg.Checkbox("Attachments", key="attachments_input")],
+        [sg.Button("Auto"), sg.Button("Manual"), sg.Button("Cancel")]]
+
+    window = sg.Window('Choose database source Operating System', layout)
+    event, values = window.read()
+    window.close()
+
+    if event == "Cancel":
+        sys.exit()
+    
+    mode = event
+    os_version = values
+    if values["attachments_input"]:
+        attachmentPath = choose_attachments_path()
+    else:
+        attachmentPath = ""
+    if values["ios"]:
+        os_version = "ios"
+        if mode == "Auto":
+            decryptedDB = ios_auto()
+        else:
+            decryptedDB = ios()
+    elif values["windows"]:
+        os_version = "windows"
+        if mode == "Auto":
+            decryptedDB = windows_auto()
+        else:
+            decryptedDB = windows()
+
+    return decryptedDB
+
+
+def ios_auto():
+    print("RUNNING IOS AUTO\n")
+    encryptedDB = "signal.sqlite"
+    keychainDump = ""
+    DB_found = False
+    error = False
+
+    for file_name in os.listdir(os.path.dirname(os.path.realpath(__file__))):
+        if file_name == encryptedDB:
+            DB_found = True
+        if file_name.enswith("signal.json") or file_name.endswith("keychain.plist"):
+            keychainDump = file_name
+
+    if not DB_found:
+        print(f"Could not find {encryptedDB}, check that the file is in the same folder as the script or run script in "
+              f"Manual mode")
+        error = True
+    if keychainDump == "":
+        print("Could not find keychain file, rename your keychain file to 'signal.json' or run script in Manual "
+              "mode")
+        error = True
+    if error:
+        os.system("pause")
+        sys.exit()
+
+    encryptedDB = os.path.realpath(encryptedDB)
+    keychainDump = os.path.realpath(keychainDump)
+
+    decryptedDB = decrypt_ios(encryptedDB, keychainDump)
+
+    return decryptedDB
+
+
+def ios():
+    print("RUNNING IOS MANUAL\n")
+    layout = [
+        [sg.Text("Signal database")],
+        [sg.In(key="database_input"), sg.FileBrowse(target="database_input", initial_folder=sys.path[0])],
+
+        [sg.Text('Keychain file')],
+        [sg.In(key="keychain_input"), sg.FileBrowse(target="keychain_input", initial_folder=sys.path[0])],
+
+        [sg.Button("Ok"), sg.Button("Cancel")]]
+
+    window = sg.Window('Choose values', layout)
+    event, values = window.read()
+    window.close()
+
+    if event == "Cancel":
+        sys.exit()
+
+    encryptedDB = ""
+    keychainDump = ""
+
+    try:
+        encryptedDB = values["database_input"]
+        keychainDump = values["keychain_input"]
+    except KeyError as E:
+        print(E)
+
+    if encryptedDB == "":
+        print("No database chosen, exiting")
+        os.system("pause")
+        sys.exit()
+    elif keychainDump == "":
+        print("No keychain chosen, exiting")
+        os.system("pause")
+        sys.exit()
+
+    print("Signal database:", encryptedDB)
+    print("Keychain file:", keychainDump)
+
+    decryptedDB = decrypt_ios(encryptedDB, keychainDump)
+
+    return decryptedDB
+
+
+def windows_auto():
+    print("RUNNING WINDOWS AUTO\n")
+    encryptedDB = "db.sqlite"
+    configfile = "config.json"
+    DB_found = False
+    config_found = False
+    error = False
+
+    for file_name in os.listdir(os.path.dirname(os.path.realpath(__file__))):
+        if file_name == encryptedDB:
+            DB_found = True
+        if file_name == configfile:
+            config_found = True
+
+    if not DB_found:
+        print(f"Could not find {encryptedDB}, check that the file is in the same folder as the script or run script in "
+              f"Manual mode")
+        error = True
+    if not config_found:
+        print(f"Could not find {configfile}, check that the file is in the same folder as the script or run script in "
+              f"Manual mode")
+        error = True
+    if error:
+        os.system("pause")
+        sys.exit()
+
+    encryptedDB = os.path.realpath(encryptedDB)
+    configfile = os.path.realpath(configfile)
+
+    decryptedDB = decrypt_windows(encryptedDB, configfile)
+
+    return decryptedDB
+
+
+def windows():
+    print("RUNNING WINDOWS\n")
+    layout = [
+        [sg.Text("Signal database")],
+        [sg.In(key="database_input"), sg.FileBrowse(target="database_input", initial_folder=sys.path[0])],
+
+        [sg.Text('Config file')],
+        [sg.In(key="config_input"), sg.FileBrowse(target="config_input", initial_folder=sys.path[0])],
+
+        [sg.Button("Ok"), sg.Button("Cancel")]]
+
+    window = sg.Window('Choose values', layout)
+    event, values = window.read()
+    window.close()
+
+    if event == "Cancel":
+        sys.exit()
+
+    encryptedDB = ""
+    configfile = ""
+
+    try:
+        encryptedDB = values["database_input"]
+        configfile = values["config_input"]
+    except KeyError as E:
+        print(E)
+
+    if encryptedDB == "":
+        print("No database chosen, exiting")
+        os.system("pause")
+        sys.exit()
+    elif configfile == "":
+        print("No keychain chosen, exiting")
+        os.system("pause")
+        sys.exit()
+
+    print("Signal database:", encryptedDB)
+    print("Keychain file:", configfile)
+
+    decryptedDB = decrypt_windows(encryptedDB, configfile)
+
+    return decryptedDB
+
+
+def decrypt_ios(encryptedDB, keychainDump):
+    print("Decrypting database")
+
+    key = get_ios_db_key(keychainDump)
+    print("Database key =", key, "\n")
+
+    decryptedDB = decrypt_db(encryptedDB, key, "ios")
+    print("Decrypted database name:", decryptedDB)
+
+    return decryptedDB
+
+
+def decrypt_windows(encryptedDB, configfile):
+    print("Decrypting database")
+
+    with open(configfile) as f:
+        config_data = json.load(f)
+
+    key = config_data["key"]
+    print("Database key =", key, "\n")
+
+    decryptedDB = decrypt_db(encryptedDB, key, "windows")
+    print("Decrypted database name:", decryptedDB)
+
+    return decryptedDB
+
+
+def get_ios_db_key(keychainDump):
+    try:
+        with open(keychainDump, 'rb') as f:
+            keychain_plist = plistlib.load(f)
+            for x in keychain_plist.values():
+                for y in x:
+                    if 'agrp' in y.keys():
+                        if y['agrp'] == b'U68MSDN6DR.org.whispersystems.signal' or \
+                                y['agrp'] == "U68MSDN6DR.org.whispersystems.signal":
+                            if y['acct'] == b'GRDBDatabaseCipherKeySpec' or y['acct'] == 'GRDBDatabaseCipherKeySpec':
+                                key = y['v_Data'].hex()
+    except:
+        with open(keychainDump, 'rb') as f:
+            json_data = json.load(f)
+            json_data = json_data[0]
+            key = json_data["dataHex"]
+    return key
+
+
+def decrypt_db(db, key, operating_system):
+    plaintextDB = os.path.splitext(db)[0] + "_decrypted.sqlite"
+    conn = sqlcipher.connect(db)
+    cur = conn.cursor()
+    if os.path.exists(plaintextDB):
+        print("Decrypted database %s already exists in destination folder. Delete it and run again." % plaintextDB)
+        os.system("pause")
+        sys.exit()
+    print("Decrypting database...")
+    cur.execute('PRAGMA key="x\'' + key + '\'"')
+    if operating_system == "ios":
+        cur.execute('PRAGMA cipher_plaintext_header_size = 32')
+    try:
+        cur.execute('SELECT count(*) FROM sqlite_master')
+    except sqlcipher.DatabaseError as E:
+        print("Decryption failed\nError:", E)
+        os.system("pause")
+        sys.exit()
+    cur.execute('ATTACH DATABASE "' + plaintextDB + '" AS plaintext KEY ""')
+    cur.execute('SELECT sqlcipher_export("plaintext")')
+    cur.execute('DETACH DATABASE plaintext')
+    print("Decryption complete")
+    conn.commit()
+    cur.close()
+
+    return plaintextDB
+    
+def choose_attachments_path():
+
+    layout = [
+            [sg.Text('Choose attachment folder')],
+            [sg.In(key="attachments_path_input"), sg.FolderBrowse(target="attachments_path_input")],
+            [sg.Button('Ok'), sg.Button('Cancel')]]
+            
+    window = sg.Window('Choose attachments folder', layout)
+    event, values = window.read()
+    window.close()
+    
+    if event == "Cancel":
+        sys.exit()
+    else:
+        attachments_path = values['attachments_path_input']
+        
+    return attachments_path
+
+
+def main():
+    global os_version
+    global attachmentPath
+    decryptedDB = main_menu()
+    report.main(decryptedDB, os_version, attachmentPath)
+
+
+if __name__ == '__main__':
+    main()
---
+++ report.py
@@ -0,0 +1,354 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import pandas as pd
+import sqlite3
+import os
+import plistlib
+import re
+import sys
+from glob import glob
+from pathlib import Path
+import pathlib
+import math
+import json
+import numpy
+from pandas.core.indexes.base import Index
+import ntpath
+from distutils.dir_util import copy_tree
+import datetime
+
+html = ""
+exe_path = os.path.dirname(os.path.realpath(__file__))
+
+
+def iOS(db, attachmentPath):
+    global html
+
+    print("Creating report")
+    db_path = os.path.dirname(os.path.realpath(db))
+    conn = sqlite3.connect(db)
+    messagesQuery = """select 
+        CASE 
+            when authorPhoneNumber is NULL then "Local User" 
+            else authorPhoneNumber 
+            end "Phone Number",
+        datetime(model_TSInteraction.receivedAtTimestamp/1000, 'unixepoch') as "Received At Timestamp", 
+        datetime(model_TSInteraction.timestamp/1000, 'unixepoch') as Timestamp,
+        CASE
+            when body is NULL then ""
+            else body
+            end Body,
+        CASE
+            when messageType is "3" then "Group Created"
+            when messageType is "7" then "Oklart"
+            when messageType is NULL then ""
+            else messageType
+            end "Message Type",
+        CASE 
+            when callType is "1" then "Incoming Call"
+            when callType is "2" then "Outgoing Call"
+            when callType is "3" then "Missed Incoming Call"
+            when callType is "7" then "Rejected Incoming Call"
+            when callType is "8" then "Outgoing Call (No answer)"
+            else ""
+            end "Call Type",
+        model_TSInteraction.attachmentIds as "Attachment",
+        CASE
+            when recordType is "9" then "Security Number Change"
+            when recordType is "10" then "Contact changed name/joined Signal"
+            when recordType is "13" then "Contact marked as verified"
+            when recordType is "19" then "Incoming Message"
+            when recordType is "20" then "Outgoing or Incoming call"
+            when recordType is "21" then "Outgoing Message"
+            when recordType is "28" then "Burn-On-Read timer changed"
+            else recordType
+            end "Record Type",
+        model_TSInteraction.uniqueThreadId, 
+        CASE
+            when read is NULL then ""
+            when read is "1" then "Yes"
+            end "Read",
+        errorType,
+        offerType,
+        infoMessageUserInfo
+        from model_TSInteraction
+        order by uniqueThreadId, timestamp
+        """
+    df = pd.read_sql_query(messagesQuery, conn)
+    df = df[df.errorType.isnull()]
+    df = df.drop(columns=['errorType'])
+    
+    getOfferType_iOS(df)
+    df = df.drop(columns=['offerType', 'infoMessageUserInfo'])
+    #if attachmentPath != "":
+    getAttachments_iOS(df, attachmentPath)
+
+    pd.set_option('display.max_colwidth', None)
+
+    for uniqueThreadId, df_uniqueThreadId in df.groupby('uniqueThreadId'):
+
+        html =  html + df_uniqueThreadId.to_html(classes = 'table-striped', escape=False, col_space=100, justify='center', index=False, formatters=dict(
+        Attachment=path_to_image_html))
+
+    getContacts_iOS(db)
+
+
+def path_to_image_html(path):
+    global attachmentPath_relative
+    global exe_path
+    global outputDir_name
+    global os_version
+    try:
+        path = path.replace("\\", "/")
+    except Exception:
+        pass
+    if path:
+        try:
+            basename = ntpath.basename(path)
+            path_with_output = outputDir_name + path[1:]
+            if os_version == "ios":
+                realpath = os.path.abspath(path_with_output)
+            else:
+                realpath = os.path.realpath(path)
+                p = pathlib.Path(path)
+                p = p.relative_to(*p.parts[:5])
+                p = "./"+str(p)
+                path = p
+            if path == exe_path or path == "":
+                return ""
+            if path == "No support for this filetype":
+                return "No support for this filetype"
+            return ('<a href="file:///' + realpath + '"><img src="' + path + '" width="150" ><br>'+basename+'</a>')
+        except:
+            return "missing attachment"
+    else:
+        return ""
+
+def getAttachments_iOS(df, attachmentPath):
+    global outputDir_name
+    attachmentPath = f"./{outputDir_name}/Attachments"
+    result = list(Path(attachmentPath).rglob("*.*"))
+    support = 0
+
+    for index, row in df.iterrows():
+        data = re.findall(
+            "[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}", str(row["Attachment"]))
+        if data:
+            support = 0
+            for image in result:
+                if data[0] in str(image):
+                    data2 = str(image)
+                    p = pathlib.Path(data2)
+                    p = p.relative_to(*p.parts[:1])
+                    p = "./"+str(p)
+                    data2 = p
+                    df.loc[index, "Attachment"] = data2
+                    support = 1
+            if support != 1:
+                df.loc[index, "Attachment"] = "Error displaying attachment"
+        else:
+            df.loc[index, "Attachment"] = ""
+            
+def getOfferType_iOS(df):
+    
+    for index, row in df.iterrows():
+        if row["offerType"] == 1:
+            callType = row["Call Type"]
+            df.loc[index, "Call Type"] = f"{callType} (Video Call)"
+            
+
+def getContacts_iOS(db):
+    global html
+
+    db_path = os.path.dirname(os.path.realpath(db))
+    conn = sqlite3.connect(db)
+    messagesQuery = """select 
+        profileName as "Profile Name",
+        CASE recipientPhoneNumber when "kLocalProfileUniqueId" then "Local User" else recipientPhoneNumber end "Phone Number",
+        datetime(lastFetchDate, 'unixepoch') as "Last Fetch Date",
+        datetime(lastMessagingDate, 'unixepoch') as "Last Messaging Date"
+        from model_OWSUserProfile
+        """
+    df = pd.read_sql_query(messagesQuery, conn)
+    
+    html = html + df.to_html(classes = 'table-striped', escape=False, col_space=200, justify='center', index=False)
+
+def windows(db, attachmentPath):
+
+    global html
+    print("Creating report")
+    db_path = os.path.dirname(os.path.realpath(db))
+    
+    conn = sqlite3.connect(db)
+    messagesQuery = """select    
+        json,
+        datetime(sent_at/1000, 'unixepoch') as "Sent At (UTC+0)", 
+        datetime(received_at/1000, 'unixepoch') as "Received At (UTC+0)",
+        conversationId,
+        type
+        from messages
+        where type = 'incoming' or type = 'outgoing'
+        order by conversationId, "Sent At (UTC+0)" 
+        """
+    acceptedTypes = ["incoming", "outgoing", "call-history"]
+    df = pd.read_sql_query(messagesQuery, conn)
+    df = df[df["type"].isin(acceptedTypes) == True]
+    df_json = getJson_windows(df)
+    df = df.drop(columns=["json"])
+    
+    if attachmentPath != "":
+        getAttachment_windows(df_json, attachmentPath)
+        
+    df_json = df_json.rename(columns={'attachments': 'Attachment'})                               #Rename columns to look like iOS html report
+    df["Attachment"] = df_json["Attachment"].values
+    df["Sender"] = df_json["source"].values
+    df["SourceUuid"] = df_json["sourceUuid"].values
+    df["Body"] = df_json["body"].values
+    
+
+    df = df[['Sender', 'Received At (UTC+0)', 'Sent At (UTC+0)', 'Body', 'Attachment', 'type', 'conversationId']]
+
+    for uniqueThreadId, df_uniqueThreadId in df.groupby('conversationId'):
+
+        html =  html + df_uniqueThreadId.to_html(classes = 'table-striped', escape=False, col_space=100, justify='center', index=False, formatters=dict(
+        Attachment=path_to_image_html))
+        
+    getContacts_windows(db)
+
+def getJson_windows(df):
+
+    json_string = "{"
+    for index, row in df.iterrows():
+        if index == 0:
+            json_string = json_string + '"' + str(index) + '":' + row["json"]
+        else:
+            json_string = json_string + ", " + '"' + str(index) + '":' + row["json"]
+    json_string = json_string + "}"
+
+    with open("messages.json", "w", encoding="UTF-8") as f:
+        f.write(json_string)
+
+    a_json = json.loads(json_string)
+    df = pd.DataFrame.from_dict(a_json, orient="index")
+    
+    os.remove("messages.json")
+
+    return df
+
+
+def getAttachment_windows(df, attachment_path):
+        
+
+    for index, row in df.iterrows():
+        full_path = ""
+        if len(row["attachments"]) != 0:
+            item = row["attachments"]
+            item = item[0]
+
+            contentType = item['contentType']
+            contentType = contentType.split("/")[1]
+            if contentType == "plain":
+                contentType = "txt"
+
+            path = item['path']
+            full_path = attachment_path + "\\" + path + "." + contentType
+            try:
+                os.rename(attachment_path + "\\" + path, attachment_path + "\\" + path + "." + contentType)
+            except FileNotFoundError:
+                pass
+   
+        if row["sticker"] is not None:
+            sticker = row["sticker"]
+            try:
+                if math.isnan(sticker) == True:
+                    df.loc[index, "attachments"] = ""
+                    continue
+            except:
+                pass
+            stickerItem = sticker['data']
+            contentType = stickerItem['contentType']
+            contentType = contentType.split("/")[1]
+            if contentType == "plain":
+                contentType = "txt"
+
+            path = stickerItem['path']
+            full_path = attachment_path + "\\" + path + "." + contentType
+            try:
+                os.rename(attachment_path + "\\" + path, attachment_path + "\\" + path + "." + contentType)
+            except FileNotFoundError:
+                pass
+        try:
+            if full_path != "":
+                df.loc[index, "attachments"] = attachment_path + "\\" + path + "." + contentType
+            else:
+                df.loc[index, "attachments"] = ""
+        except:
+            pass
+            
+def getContacts_windows(db):
+    global html
+
+    db_path = os.path.dirname(os.path.realpath(db))
+    conn = sqlite3.connect(db)
+    messagesQuery = """
+    select
+    CASE
+    when profileFullName is NULL then name
+    else profileFullName
+    END as "Profile Name",
+    e164 as "Phone Number",
+    datetime(active_at, 'unixepoch') as "Last Active Date",
+    id as conversationId
+    from conversations
+    """
+
+    df = pd.read_sql_query(messagesQuery, conn)
+
+
+    html = html + df.to_html(classes = 'table-striped', escape=False, col_space=200, justify='center', index=False)
+
+def main(db, os_version1, attachmentPath):
+    global os_version
+    global attachmentPath_relative
+    global outputDir
+    global outputDir_name
+    os_version = os_version1
+    print("OS VERSION", os_version)
+    print("DATABASE", db)
+    print("ATTACHMENT PATH", attachmentPath)
+    print("")
+    
+    db_path = os.path.dirname(db)
+    outputDir = db_path + "/" + "Signal_report" + '_' + datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
+    outputDir_name = os.path.basename(outputDir)
+
+    try:
+        if not os.path.exists(outputDir):
+            os.makedirs(outputDir)
+    except ValueError:
+        print("Could not create outdirectory")
+        os.system("pause")
+        sys.exit()
+    if attachmentPath != "":
+        copy_tree(attachmentPath, outputDir+"//Attachments")
+        attachmentPath = outputDir+"//Attachments"
+        attachmentPath_relative = "//Attachments"
+    
+    if os_version == "ios":
+        iOS(db, attachmentPath)
+    else:
+        windows(db, attachmentPath)
+
+    # write html to file
+    text_file = open(f"{outputDir}/report_{os_version}.html", "w")
+    text_file.write(html)
+    text_file.close()
+    
+    print(f"Report created, {outputDir}/report_{os_version}.html")
+    
+if __name__ == '__main__':
+    print(sys.argv[1:])
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
+    
+    
